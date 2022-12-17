f224323a5c2495a6d4e64da4d7df64b7880c8f2b
skubaty <szkubaty@student.agh.edu.pl>
2022-12-17 18:42:15 +0100

    Jakie zamówienia są na wynos

--- Views/views2.sql
+++ Views/views2.sql
@@ -1,6 +1,21 @@
 create view dbo.Waiter as
     select FirstName + ' ' + LastName as Name, OrderID as id
     from Staff
-         join Orders O on Staff.StaffID = O.staffID
+             join Orders O on Staff.StaffID = O.staffID
     where Position = 'waiter'
-        or Position = 'waitress';
+       or Position = 'waitress';
+
+create view dbo.Takeaways as
+    select TakeawayID,
+           PrefDate,
+           OrderID,
+           ClientID,
+           PaymentStatusID,
+           staffID,
+           OrderSum,
+           OrderDate,
+           OrderCompletionDate,
+           OrderStatus
+    from OrdersTakeaways
+             join Orders O on OrdersTakeaways.TakeawaysID = O.TakeawayID;
+
