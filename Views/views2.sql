a6b52c65c6698cf0921e2ad7cbbfde254ba24f6d
skubaty <szkubaty@student.agh.edu.pl>
2022-12-17 19:10:58 +0100

    Jakie zamówienie ma do odebrania dany klient

--- Views/views2.sql
+++ Views/views2.sql
@@ -33,3 +33,9 @@ create view SeeFood as
     select OD.OrderID
     from Orders join OrderDetails OD on Orders.OrderID = OD.OrderID join Products P on P.ProductID = OD.ProductID join Category C on C.CategoryID = P.CategoryID
     where CategoryName='sea food' and OrderCompletionDate is null
+
+create view OrdersNotYetPickedByClient as
+    select ClientID,TakeawaysID,OrderID
+    from Clients join Orders O on Clients.ClientID = O.ClientID join OrdersTakeaways OT on OT.TakeawaysID = O.TakeawayID
+    where O.Picked=FALSE
+    order by ClientID
