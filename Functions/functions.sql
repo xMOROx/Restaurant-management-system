48be401ef6f504dbeffe664941c7fb5c80aa4b69
xMOROx <zajdel0202@gmail.com>
2022-12-31 00:42:31 +0100

    Correctness of menu

--- Functions/functions.sql
+++ Functions/functions.sql
@@ -5,3 +5,29 @@ AS
         RETURN (SELECT AVG(Price) FROM Menu WHERE MenuID = @MenuID)
     END
 GO
+
+CREATE FUNCTION MenuIsCorrect(@MenuID int)
+    RETURNS bit
+AS
+    BEGIN
+        DECLARE @SameItems int 
+        SET @SameItems = (SELECT COUNT(*) 
+        FROM (
+                SELECT ProductID 
+                FROM Menu 
+                WHERE MenuID = (@MenuID - 1)
+                INTERSECT
+                SELECT ProductID
+                FROM Menu
+                WHERE MenuID = @MenuID
+        ) OUT)
+
+        DECLARE @minAmountToChange int 
+        SET @minAmountToChange = (SELECT COUNT(*) FROM Menu WHERE MenuID = (@MenuID - 1)) / 2
+        IF @SameItems <= @minAmountToChange
+            BEGIN
+                RETURN 1
+            END
+        RETURN 0
+    END
+GO
