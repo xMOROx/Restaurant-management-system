a4e4fe131392c268d597b7896bdb78278a148d1c
xMOROx <zajdel0202@gmail.com>
2022-12-31 01:29:06 +0100

    Table functions

--- Functions/functions.sql
+++ Functions/functions.sql
@@ -47,3 +47,40 @@ AS
         RETURN (SELECT TOP 1 MAX(Price) FROM Menu WHERE MenuID = @MenuID)
     END
 GO
+
+CREATE FUNCTION show_taken_tables_from_x_to_y_with_z_chairs(
+    @StartDate datetime,
+    @EndDate datetime,
+    @Chairs int
+)
+    RETURNS TABLE
+AS
+    RETURN
+        SELECT T.TableID, T.ChairAmount, O.ClientID, O.OrderID, R2.startDate, R2.endDate  FROM Tables T
+            INNER JOIN ReservationDetails RD on T.TableID = RD.TableID
+            INNER JOIN ReservationCompany RC on RC.ReservationID = RD.ReservationID
+            INNER JOIN Reservation R2 on RC.ReservationID = R2.ReservationID
+            INNER JOIN Orders O on R2.ReservationID = O.ReservationID
+        WHERE R2.startDate >= @StartDate AND R2.endDate <= @EndDate AND T.ChairAmount = @Chairs
+    UNION
+        SELECT T.TableID, T.ChairAmount, O.ClientID, O.OrderID, R2.startDate, R2.endDate FROM Tables T
+            INNER JOIN ReservationDetails RD on T.TableID = RD.TableID
+            INNER JOIN ReservationIndividual RC on RC.ReservationID = RD.ReservationID
+            INNER JOIN Reservation R2 on RC.ReservationID = R2.ReservationID
+            INNER JOIN Orders O on R2.ReservationID = O.ReservationID
+        WHERE R2.startDate >= @StartDate AND R2.endDate <= @EndDate AND T.ChairAmount = @Chairs
+go
+
+CREATE FUNCTION show_free_tables_from_x_to_y_with_z_chairs(
+    @StartDate datetime,
+    @EndDate datetime,
+    @Chairs int
+)
+    RETURNS TABLE
+AS
+    RETURN
+    SELECT T.TableID, T.ChairAmount FROM Tables T
+    WHERE T.TableID NOT IN (
+        SELECT Q.TableID FROM show_taken_tables_from_x_to_y_with_z_chairs(@StartDate, @EndDate, @Chairs) Q
+    ) AND T.isActive = 1 AND ChairAmount=@Chairs
+GO
