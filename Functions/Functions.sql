b39cf5339a51f30ee35c8d4555bf43b45465ad18
skubaty <szkubaty@student.agh.edu.pl>
2023-01-02 21:40:15 +0100

    nowe funkcje

--- Functions/Functions.sql
+++ Functions/Functions.sql
@@ -1,9 +1,33 @@
+CREATE FUNCTION calculateBestDiscountTemporary(@ClientID int) RETURNS decimal(3, 2) AS
+BEGIN
+    RETURN (SELECT max(DiscountValue)
+            FROM IndividualClient I
+                     JOIN Discounts D ON I.ClientID = D.ClientID
+                     JOIN DiscountsVar DV ON DV.VarID = D.VarID
+            WHERE DiscountType = 'Temporary'
+              AND I.ClientID = @ClientID
+              AND AppliedDate <= getdate() <= dateadd(DAY, ValidityPeriod, AppliedDate) -- Temporary Discounts must have endDate
+    )
+END
+GO
+
+CREATE FUNCTION calculateBestDiscountPermanent(@ClientID int) RETURNS decimal(3, 2) AS
+BEGIN
+    RETURN (SELECT max(DiscountValue)
+            FROM IndividualClient I
+                     JOIN Discounts D ON I.ClientID = D.ClientID
+                     JOIN DiscountsVar DV ON DV.VarID = D.VarID
+            WHERE DiscountType = 'Permanent'
+              AND I.ClientID = @ClientID)
+END
+GO
+
 CREATE FUNCTION calculateDiscountForOrder(@OrderId int) RETURNS money AS
 BEGIN
-    RETURN
-    SELECT (showBestDiscountPermanent(ClientID) + showBestDiscountTemporary(ClientID)) * Orders.OrderSum
-    FROM Orders
-    WHERE OrderID = @OrderId
+    RETURN (SELECT (calculateBestDiscountTemporary(ClientID) + calculateBestDiscountPermanent(ClientID)) *
+                   Orders.OrderSum
+            FROM Orders
+            WHERE OrderID = @OrderId)
 END
 GO
 ;
