0ad10f54c192116de7591baf5c9d3eb69551c676
xMOROx <zajdel0202@gmail.com>
2023-01-02 23:32:38 +0100

    Procedures and Functions about clients

--- Functions/functions.sql
+++ Functions/functions.sql
@@ -166,3 +166,55 @@ FROM
 WHERE
     [money to pay] > @value
 GO
+    CREATE FUNCTION calculateBestDiscountTemporary(@ClientID int) RETURNS decimal(3, 2) AS BEGIN RETURN (
+        SELECT
+            max(DiscountValue)
+        FROM
+            IndividualClient I
+            JOIN Discounts D ON I.ClientID = D.ClientID
+            JOIN DiscountsVar DV ON DV.VarID = D.VarID
+        WHERE
+            DiscountType = 'Temporary'
+            AND I.ClientID = @ClientID
+            AND AppliedDate <= getdate() <= dateadd(DAY, ValidityPeriod, AppliedDate) -- Temporary Discounts must have endDate
+    )
+END
+GO
+    CREATE FUNCTION calculateBestDiscountPermanent(@ClientID int) RETURNS decimal(3, 2) AS BEGIN RETURN (
+        SELECT
+            max(DiscountValue)
+        FROM
+            IndividualClient I
+            JOIN Discounts D ON I.ClientID = D.ClientID
+            JOIN DiscountsVar DV ON DV.VarID = D.VarID
+        WHERE
+            DiscountType = 'Permanent'
+            AND I.ClientID = @ClientID
+    )
+END
+GO
+    CREATE FUNCTION calculateDiscountForOrder(@OrderId int) RETURNS money AS BEGIN RETURN (
+        SELECT
+            (
+                calculateBestDiscountTemporary(ClientID) + calculateBestDiscountPermanent(ClientID)
+            ) * Orders.OrderSum
+        FROM
+            Orders
+        WHERE
+            OrderID = @OrderId
+    )
+END
+GO
+;
+
+CREATE FUNCTION sumOfMoneySpentIn_Month_Year(@WhichYear int, @WhichMonth int) RETURNS money AS BEGIN RETURN (
+    SELECT
+        sum(OrderSum)
+    FROM
+        dbo.Orders
+    WHERE
+        @WhichYear = Year(OrderDate)
+        AND @WhichMonth = MONTH(OrderDate)
+)
+END
+GO
--- Procedures/Procedures.sql
+++ Procedures/Procedures.sql
@@ -1186,3 +1186,161 @@ END catch
 END;
 
 GO
+  CREATE PROCEDURE addCity @CityName nvarchar(35) AS
+INSERT INTO
+  Cities (CityName)
+VALUES
+  (@CityName);
+
+GO
+;
+
+CREATE PROCEDURE addStaffMember @LastName nvarchar(50),
+@FirstName nvarchar(70),
+@Position varchar(50),
+@Email varchar(100),
+@Phone varchar(14),
+@AddressID int AS
+INSERT INTO
+  Staff (
+    LastName,
+    FirstName,
+    Position,
+    Email,
+    Phone,
+    AddressID
+  )
+VALUES
+  (
+    @LastName,
+    @FirstName,
+    @Position,
+    @Email,
+    @Phone,
+    @AddressID
+  );
+
+GO
+;
+
+CREATE PROCEDURE addCategory @CategoryName nvarchar(50),
+@Description nvarchar(150) AS
+INSERT INTO
+  Category (CategoryName, Description)
+VALUES
+  (@CategoryName, @Description);
+
+GO
+;
+
+CREATE PROCEDURE addProduct @CategoryID int,
+@Name nvarchar(50),
+@Description nvarchar(150),
+@IsAvailable bit AS
+INSERT INTO
+  Products (CategoryID, Name, Description, IsAvailable)
+VALUES
+  (@CategoryID, @Name, @Description, @IsAvailable);
+
+GO
+;
+
+CREATE PROCEDURE removeCity @CityID int AS
+DELETE FROM
+  Cities
+WHERE
+  CityID = @CityID
+GO
+;
+
+CREATE PROCEDURE removeCategory @CategoryID int AS
+DELETE FROM
+  Category
+WHERE
+  CategoryID = @CategoryID
+GO
+;
+
+CREATE PROCEDURE removeProduct @ProductID int AS
+DELETE FROM
+  Products
+WHERE
+  ProductID = @ProductID
+GO
+;
+
+-- null->accepted->pending->completed->picked (jesli na wynos)
+-- null->denied (zwrot srodkow)
+CREATE PROCEDURE denyOrder @OrderID int AS
+UPDATE
+  Orders
+SET
+  OrderStatus = 'denied'
+WHERE
+  OrderID = @OrderID;
+
+GO
+;
+
+-- pracownicy mają working hours, muszą chyba kiedyś spać?
+CREATE PROCEDURE AssignWaiterToOrder @OrderID int,
+@StaffId int AS
+UPDATE
+  Orders
+SET
+  StaffId = @StaffId
+WHERE
+  OrderID = @OrderID;
+
+GO
+;
+
+-- who accepts reservation or denies it
+CREATE PROCEDURE AssignReservationManOrWomanToResrvation @ReservationID int,
+@StaffId int AS
+UPDATE
+  Reservation
+SET
+  StaffId = @StaffId
+WHERE
+  ReservationID = @ReservationID;
+
+GO
+;
+
+-- jeden payment dotyczy wielu zamowien, co jezeli tylko czesc z nich jest anulowana, a czesc nie jest...
+CREATE PROCEDURE refundWasMade @PaymentStatusID int AS
+UPDATE
+  PaymentStatus
+SET
+  PaymentStatusName = 'Refunded'
+WHERE
+  PaymentStatusID = @PaymentStatusID;
+
+GO
+;
+
+CREATE PROCEDURE showBestDiscountTemporary @ClientID int AS
+SELECT
+  max(DiscountValue)
+FROM
+  IndividualClient I
+  JOIN Discounts D ON I.ClientID = D.ClientID
+  JOIN DiscountsVar DV ON DV.VarID = D.VarID
+WHERE
+  DiscountType = 'Temporary'
+  AND I.ClientID = @ClientID
+  AND AppliedDate <= getdate() <= dateadd(DAY, ValidityPeriod, AppliedDate) -- Temporary Discounts must have endDate
+GO
+  CREATE PROCEDURE showBestDiscountPermanent @ClientID int AS
+SELECT
+  max(DiscountValue)
+FROM
+  IndividualClient I
+  JOIN Discounts D ON I.ClientID = D.ClientID
+  JOIN DiscountsVar DV ON DV.VarID = D.VarID
+WHERE
+  DiscountType = 'Permanent'
+  AND I.ClientID = @ClientID
+GO
+;
