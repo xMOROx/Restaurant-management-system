5c49e967a78a8cc815984f4cbbc51dc7e04bced0
xMOROx <zajdel0202@gmail.com>
2023-01-11 22:33:08 +0100

    Repair functions because of new scheme

--- Functions/functions.sql
+++ Functions/functions.sql
@@ -2,63 +2,19 @@ CREATE FUNCTION GetAvgPriceOfMenu(@MenuID int) RETURNS money AS BEGIN RETURN (
     SELECT
         AVG(Price)
     FROM
-        Menu
+        MenuDetails
     WHERE
         MenuID = @MenuID
 ) END
 GO
-    CREATE FUNCTION MenuIsCorrect(@MenuID int) RETURNS bit AS BEGIN DECLARE @SameItems int
-SET
-    @SameItems = (
-        SELECT
-            COUNT(*)
-        FROM
-            (
-                SELECT
-                    ProductID
-                FROM
-                    Menu
-                WHERE
-                    MenuID = (@MenuID - 1)
-                INTERSECT
-                SELECT
-                    ProductID
-                FROM
-                    Menu
-                WHERE
-                    MenuID = @MenuID
-            ) OUT
-    ) DECLARE @minAmountToChange int
-SET
-    @minAmountToChange = (
-        SELECT
-            COUNT(*)
-        FROM
-            Menu
-        WHERE
-            MenuID = (@MenuID - 1)
-    ) / 2 IF @SameItems <= @minAmountToChange BEGIN RETURN 1 END RETURN 0 END
-GO
 
---helps to make a new menu
-CREATE FUNCTION WhatWasNotInTheMenuOfGivenID(@MenuID int)
-    RETURNS TABLE AS RETURN
-        SELECT P.ProductID, P.Name, P.Description as 'Product Description', P.IsAvailable, C.CategoryName , C.Description as 'Category Description' FROM Products P
-                INNER JOIN Category C on C.CategoryID = P.CategoryID
-            WHERE P.ProductID IN
-        (SELECT ProductID
-         FROM Products PI
-         EXCEPT
-         SELECT ProductID
-         FROM Menu
-         WHERE MenuID=@MenuID)
-GO
+
 
 CREATE FUNCTION GetMinimumPriceOfMenu(@MenuID int) RETURNS money AS BEGIN RETURN (
         SELECT
             TOP 1 MIN(Price)
         FROM
-            Menu
+            MenuDetails
         WHERE
             MenuID = @MenuID
     ) END
@@ -67,7 +23,7 @@ CREATE FUNCTION GetMaximumPriceOfMenu(@MenuID int) RETURNS money AS BEGIN RETURN
         SELECT
             TOP 1 MAX(Price)
         FROM
-            Menu
+            MenuDetails
         WHERE
             MenuID = @MenuID
     ) END
@@ -188,7 +144,7 @@ FROM
 WHERE
     [money to pay] > @value
 GO
-
+-- Jeżeli zniżka tymczasowa i została użyta to zmień jej pole isUsed na 1
 
 CREATE FUNCTION calculateBestDiscountTemporary(@ClientID int) RETURNS decimal(3, 2) AS BEGIN RETURN (
         SELECT
@@ -200,6 +156,7 @@ CREATE FUNCTION calculateBestDiscountTemporary(@ClientID int) RETURNS decimal(3,
         WHERE
             DiscountType = 'Temporary'
             AND I.ClientID = @ClientID
+            AND isUsed = 0
             AND AppliedDate <= getdate()
             AND getdate() <= dateadd(DAY, ValidityPeriod, AppliedDate) -- Temporary Discounts must have endDate
     ) END
@@ -220,8 +177,8 @@ CREATE FUNCTION calculateBestDiscountPermanent(@ClientID int) RETURNS decimal(3,
 GO
 
 
-CREATE FUNCTION calculateDiscountForOrder(@OrderId int) RETURNS money 
-AS BEGIN 
+CREATE FUNCTION calculateDiscountForOrder(@OrderId int) RETURNS money
+AS BEGIN
 DECLARE @BestValue decimal(3, 2);
 DECLARE @ClientID int;
 SET
@@ -257,53 +214,129 @@ CREATE FUNCTION sumOfMoneySpentIn_Month_Year(@WhichYear int, @WhichMonth int) RE
     ) END
 GO
 
--- Zwracanie informacji o zamówieniu o podanym indeksie 
+-- Zwracanie informacji o zamówieniu o podanym indeksie
 
-CREATE FUNCTION GetOrderDetails(@InputOrderID int) RETURNS TABLE AS RETURN (
+CREATE FUNCTION GetOrderDetails(@InputOrderID int) RETURNS TABLE
+    AS
+        RETURN (
         SELECT
-            O.*
-        FROM
-            Orders AS O
+            O.OrderID,
+            O.ClientID,
+            ISNULL(O.TakeawayID, 'Order not for takeaway') as 'TakeAwayID',
+            ISNULL(O.ReservationID, 'Order is not for reservation') as 'ReservationID',
+            ISNULL(O.InvoiceID, 'Order does not have invoice.') as 'InvoiceID',
+            PM.PaymentName,
+            PS.PaymentStatusName,
+            CONCAT(S.LastName, ' ', S.FirstName) as 'Employee',
+            O.OrderSum,
+            O.OrderDate,
+            ISNULL(O.OrderCompletionDate, 'Order is pending') as 'OrderCompletionDate',
+            O.OrderStatus,
+            P.Name,
+            (SELECT MD.Price FROM MenuDetails MD INNER JOIN CurrentMenu CM on MD.MenuID = CM.MenuID WHERE MD.ProductID = OD.ProductID) as 'Product Price',
+            OD.Quantity
+        FROM Orders O
+            INNER JOIN OrderDetails OD on O.OrderID = OD.OrderID
+            INNER JOIN Products P ON P.ProductID = OD.ProductID
+            INNER JOIN PaymentMethods PM on O.PaymentMethodID = PM.PaymentMethodID
+            INNER JOIN PaymentStatus PS on O.PaymentStatusID = PS.PaymentStatusID
+            INNER JOIN Staff S on O.staffID = S.StaffID
         WHERE
-            OrderID = InputOrderID
-    ) END 
-    
--- Zwracanie informacji o produkcie o podanej nazwie(informacje ile było zamówiony wciągu 14 dni) 
-CREATE FUNCTION OrderProductWithin14 days(@InputProductName nvarchar(150)) RETURNS INT AS BEGIN RETURN (
+            O.OrderID = @InputOrderID
+    )
+GO
+
+-- Zwracanie informacji o produkcie o podanej nazwie(informacje ile było zamówiony wciągu 14 dni)
+CREATE FUNCTION OrderProductWithin14days (@InputProductName nvarchar(150)) RETURNS INT
+AS
+    BEGIN
+     RETURN (
         SELECT
-            SUM [O D].Quantity
+            SUM([O D].Quantity)
         FROM
             OrderDetails AS [O D]
             INNER JOIN Products P ON P.ProductID = [O D].ProductID
             INNER JOIN Orders O ON O.OrderID = [O D].OrderID
         WHERE
-            P.Name LIKE InputProductName
-            AND DATEDIFF(DAY, O.OrderDate, GETDATE())
-    ) END
-    
--- Informacje o zamówieniach powyżej ceny X 
-CREATE FUNCTION OrdersMoreExpensiveThanN RETURNS TABLE AS RETURN (
+            P.Name LIKE @InputProductName
+            AND ABS(DATEDIFF(DAY, O.OrderDate, GETDATE())) <= 14
+    )
+    END
+GO
+
+-- Informacje o zamówieniach powyżej ceny X
+CREATE FUNCTION OrdersMoreExpensiveThanN (@N int) RETURNS TABLE
+AS
+    RETURN (
         SELECT
             O.*
         FROM
             Orders AS O
         WHERE
-            O.OrderSum > N
+            O.OrderSum > @N
     )
+GO
 
 CREATE FUNCTION ShowDuplicatedProductsInXMenuFromYMenu(@MenuFirstID int, @MenuSecondID int)
     RETURNS TABLE
         AS RETURN SELECT max(M.MenuID) as 'MenuID', P.ProductID, P.Name, P.Description FROM Products P
             INNER JOIN (SELECT P1.ProductID, P1.Name, P1.Description
-                FROM Menu M1
+                FROM MenuDetails M1
                     INNER JOIN Products P1 on P1.ProductID = M1.ProductID
                 WHERE MenuID = (@MenuSecondID)
                     INTERSECT
                 SELECT P2.ProductID, P2.Name, P2.Description
-                FROM Menu M2
+                FROM MenuDetails M2
                     INNER JOIN Products P2 on P2.ProductID = M2.ProductID
-                WHERE MenuID = @MenuFirstID) P3 
+                WHERE MenuID = @MenuFirstID) P3
                 ON P3.ProductID = P.ProductID
-                INNER JOIN Menu M on P.ProductID = M.ProductID
+                    INNER JOIN MenuDetails M on P.ProductID = M.ProductID
                 GROUP BY P.ProductID, P.Name, P.Description
-go
+GO
+
+--helps to make a new menu
+CREATE FUNCTION WhatWasNotInTheMenuOfGivenID(@MenuID int)
+    RETURNS TABLE AS RETURN
+        SELECT P.ProductID, P.Name, P.Description as 'Product Description', P.IsAvailable, C.CategoryName , C.Description as 'Category Description' FROM Products P
+                INNER JOIN Category C on C.CategoryID = P.CategoryID
+            WHERE P.ProductID IN
+        (SELECT ProductID
+         FROM Products PI
+         EXCEPT
+         SELECT ProductID
+         FROM Menu
+         WHERE MenuID=@MenuID)
+GO
+
+    CREATE FUNCTION MenuIsCorrect(@MenuID int) RETURNS bit AS BEGIN DECLARE @SameItems int
+SET
+    @SameItems = (
+        SELECT
+            COUNT(*)
+        FROM
+            (
+                SELECT
+                    ProductID
+                FROM
+                    MenuDetails
+                WHERE
+                    MenuID = (@MenuID - 1)
+                INTERSECT
+                SELECT
+                    ProductID
+                FROM
+                    MenuDetails
+                WHERE
+                    MenuID = @MenuID
+            ) OUT
+    ) DECLARE @minAmountToChange int
+SET
+    @minAmountToChange = (
+        SELECT
+            COUNT(*)
+        FROM
+            MenuDetails
+        WHERE
+            MenuID = (@MenuID - 1)
+    ) / 2 IF @SameItems <= @minAmountToChange BEGIN RETURN 1 END RETURN 0 END
+GO
